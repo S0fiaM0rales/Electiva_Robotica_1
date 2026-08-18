760e75336ae93de3f6f003badd643d3fc3993a91
Sofía Morales <127352814+S0fiaM0rales@users.noreply.github.com>
2026-08-18 18:46:40 -0500

    Update punto_1_vectores.py

--- punto_1_vectores.py
+++ punto_1_vectores.py
@@ -1,1 +1,44 @@
+import numpy as np
 
+vector_A = np.array([2, 4, 6])
+vector_B = np.array([1, 3, 5])
+
+# Suma
+suma = vector_A + vector_B
+
+# Resta
+resta = vector_A - vector_B
+
+# Producto elemento a elemento
+producto = vector_A * vector_B
+
+# Producto punto
+producto_punto = np.dot(vector_A, vector_B)
+
+# Producto cruz
+producto_cruz = np.cross(vector_A, vector_B)
+
+# División elemento a elemento
+division = vector_A / vector_B
+
+# RESULTADOS
+print("VECTOR A:", vector_A)
+print("VECTOR B:", vector_B)
+
+print("\nSuma:")
+print(suma)
+
+print("\nResta:")
+print(resta)
+
+print("\nProducto elemento a elemento:")
+print(producto)
+
+print("\nProducto punto:")
+print(producto_punto)
+
+print("\nProducto cruz:")
+print(producto_cruz)
+
+print("\nDivisión elemento a elemento:")
+print(division)
