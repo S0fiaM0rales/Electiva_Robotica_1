c467021d6d672e6d4dc5fe63e61cce6bc8c81ed5
Sofía Morales <127352814+S0fiaM0rales@users.noreply.github.com>
2026-08-18 19:22:15 -0500

    Update punto_4_pt100.py

--- punto_4_pt100.py
+++ punto_4_pt100.py
@@ -11,12 +11,22 @@ T = 25
 # Cálculo de la resistencia usando
 # la ecuación de Callendar-Van Dusen
 
-R = R0 * (
-    1
-    + A * T
-    + B * T**2
-    + C * (T - 100) * T**3
-)
+if T >= 0:
+    # Para temperaturas iguales o mayores a 0 °C
+    R = R0 * (
+        1
+        + A * T
+        + B * T**2
+    )
+
+else:
+    # Para temperaturas menores a 0 °C
+    R = R0 * (
+        1
+        + A * T
+        + B * T**2
+        + C * (T - 100) * T**3
+    )
 
 # Mostrar el resultado
 print(
